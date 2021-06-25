aebb34480b7162804df95bd512cf4255241cb143
navdeeppatel1852 <navdeeppatel.1852@gmail.com>
2021-06-25 23:25:37 +0530

    change port

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const bodyParser = require("body-parser");
 
 const adminRouter = require("./routes/admin");
 
+const port = process.env.port || 3001;
 const app = express();
 app.use(bodyParser.json());
 
 app.use("/admin", adminRouter);
 
-app.listen(3001, () => {
+app.listen(port, () => {
   console.log("Server up at 3001");
 });
