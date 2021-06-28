0df47ab6672e2c8bfbdf91d2fa1f32655e0ce213
navdeeppatel1852 <navdeeppatel.1852@gmail.com>
2021-06-28 15:41:45 +0530

    change cors

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const adminRouter = require("./routes/admin");
 const port = process.env.PORT || 3001;
 
 const app = express();
-app.use(bodyParser.json());
 
 app.use(cors());
 
@@ -23,6 +22,9 @@ app.use((req, res, next) => {
   }
   next();
 });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(isAuth);
 
