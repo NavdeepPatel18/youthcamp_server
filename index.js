c9d75b570187660b22220f8ca38fdc950485b460
navdeeppatel1852 <navdeeppatel.1852@gmail.com>
2021-06-28 16:29:18 +0530

    change

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPITIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-type, Authorization,Origin, X-Requested-With,Accept");
+  res.setHeader("Access-Control-Allow-Headers", "Content-type");
   // res.setHeader("Access-Control-Allow-Credentials", true);
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);
