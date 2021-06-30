e7c699128f0c07476f0fb29d417c95d89d398ada
navdeeppatel1852 <navdeeppatel.1852@gmail.com>
2021-06-30 11:35:22 +0530

    debuging........

--- controllers/ContactUsController.js
+++ controllers/ContactUsController.js
@@ -43,7 +43,7 @@ module.exports = class ContactUsController extends BaseController {
       });
 
       if (type) {
-        if (req.files[0]) {
+        if (req.body.teamMemberPhoto) {
           var i = 0;
           teamMember.forEach((element) => {
             element.photo =
